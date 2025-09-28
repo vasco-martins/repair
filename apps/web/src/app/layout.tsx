290a792a8f50f4c78401306eef6cff302f1ca1db
Vasco Martins <vascomartins37@gmail.com>
2025-09-28 04:49:49 +0100

    feat(web): add shadcn components with Tailwind CSS

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Toaster } from 'sonner'
 import './globals.css'
 
 import type { Metadata } from 'next'
@@ -13,7 +14,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`antialiased`}>{children}</body>
+      <body className={`antialiased`}>
+        {children} 
+        <Toaster/>
+      </body>
     </html>
   )
 }
