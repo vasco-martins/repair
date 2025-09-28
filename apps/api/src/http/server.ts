f5bfa21b499c6b3ca8674237713874206ae8d20d
Vasco Martins <vascomartins37@gmail.com>
2025-09-28 12:35:55 +0100

    feat(api): integrate Swagger documentation and update server configuration

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -1,19 +1,41 @@
 import fastifyCors from '@fastify/cors'
+import fastifySwagger from '@fastify/swagger'
+import fastifySwaggerUI from '@fastify/swagger-ui'
 import { fastify } from 'fastify'
 import {
-  // jsonSchemaTransform,
+  jsonSchemaTransform,
   serializerCompiler,
   validatorCompiler,
   ZodTypeProvider,
 } from 'fastify-type-provider-zod'
+
+import { createAccount } from './routes/auth/create-account'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
 
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Repair API',
+      description: 'Repair backend service',
+      version: '1.0.0',
+    },
+    servers: [],
+  },
+  transform: jsonSchemaTransform,
+})
+
+app.register(fastifySwaggerUI, {
+  routePrefix: '/documentation',
+})
+
 app.register(fastifyCors)
 
-app.listen({ port: 3000 }).then(() => {
-  console.log('Server is running on port 3000')
+app.register(createAccount)
+
+app.listen({ port: 8000 }).then(() => {
+  console.log('Server is running on port 8000')
 })
